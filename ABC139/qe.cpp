ef1275bda484f8303fb94aa889d73e838cdc0179
hongu-ku <hongu.sol.0901@gmail.com>
2019-09-02 22:05:42 +0900

    abc139/qe.cppのコメントアウトを削除

--- ABC139/qe.cpp
+++ ABC139/qe.cpp
@@ -11,17 +11,13 @@ int main () {
   scanf("%d",&n);
   int a[n][n];
   std::vector<int> v(n,0);
-  // bool flag[n];
-  // cout << "debug" << endl;
   rep(i,n) {
-    // flag[i] = false;
     rep(j,n-1) {
       cin >> k;
       a[i][j] = k-1;
     }
     a[i][n-1] = -1;
   }
-  // cout << "debug" << endl;
   bool today[n];
   int count = 0, days = 0;
   while(count < n*(n-1)/2) {
@@ -31,18 +27,12 @@ int main () {
       today[i] = false;
     }
     rep(i,n) {
-      // if(days == 4 && flag[i] == true)  cout << i << " :: " << flag[i] << endl;
       if(today[i] == true) continue;
       if (a[a[i][v[i]]][v[a[i][v[i]]]] == i && a[i][v[i]] > i && today[a[i][v[i]]] == false/* && flag[i] == false && flag[a[i][v[i]]] == false*/) {
         today[i] = true; today[a[i][v[i]]] = true;
         count++;
-        // cout << i << " VS " << a[i][v[i]] << ", count = " << count << ", days = " << days << endl;
-        // if(v[a[i][v[i]]] < n-1)
         v[a[i][v[i]]]++;
-        // else {flag[a[i][v[i]]] = true;
-        // if(v[i] < n-1)
         v[i]++;
-        // else flag[i] = true;
       }
     }
     rep(i,n) {
