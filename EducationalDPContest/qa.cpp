58aae04bdcc09d39bb873ddd8361fb3a773a283b
hongu-ku <hongu.sol.0901@gmail.com>
2019-11-09 15:26:36 +0900

    DP練習

---
+++ EducationalDPContest/qa.cpp
@@ -0,0 +1,71 @@
+// 動的計画法
+
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define INF 999999999
+#define size_t unsigned long long
+#define ll long long
+#define rep(i,a) for(int i=0;i<(a);i++)
+#define repr(i,a) for(int i=(int)(a)-1;i>=0;i--)
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define FORR(i,a,b) for(int i=(int)(b)-1;i>=a;i--)
+#define ALL(a) a.begin(), a.end()
+using namespace std;
+int si() { int x; scanf("%d", &x); return x; }
+long long sl() { long long x; scanf("%lld", &x); return x; }
+string ss() { string x; cin >> x; return x; }
+void pi(int x) { printf("%d ", x); }
+void pl(long long x) { printf("%lld ", x); }
+void pd(double x) { printf("%.15f ", x); }
+void ps(const string &s) { printf("%s ", s.c_str()); }
+void br() { putchar('\n'); }
+
+const int MOD = 1e9 + 7;
+
+struct mint {
+    int n;
+    mint(int n_ = 0) : n(n_) {}
+};
+
+mint operator+(mint a, mint b) { a.n += b.n; if (a.n >= MOD) a.n -= MOD; return a; }
+mint operator-(mint a, mint b) { a.n -= b.n; if (a.n < 0) a.n += MOD; return a; }
+mint operator*(mint a, mint b) { return (long long)a.n * b.n % MOD; }
+mint &operator+=(mint &a, mint b) { return a = a + b; }
+mint &operator-=(mint &a, mint b) { return a = a - b; }
+mint &operator*=(mint &a, mint b) { return a = a * b; }
+
+const int n_max = 1e5 + 5;
+
+int n;
+int h[n_max];
+
+int dp[n_max]; //足場iにいるときのコストの最小値
+
+int rec(int i) {
+  if(i==0) dp[i] = 0;
+  else if(i==1) dp[i] = abs(h[i] - h[i-1]);
+  else dp[i] = min(dp[i-2] + abs(h[i] - h[i-2]), dp[i-1] + abs(h[i] - h[i-1]));
+  if(i<n-1) rec(i+1);
+  return 0;
+}
+
+int main() {
+  rep(i,n_max) dp[i] = INF;
+  cin >> n;
+  rep(i,n) cin >> h[i];
+  rec(0);
+  cout << dp[n-1] << endl;
+}
